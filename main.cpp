b6094d0beef2f3c6fc6afa8ec00d852b6a294a26
hosternus <90009071+hosternus@users.noreply.github.com>
2025-01-24 05:24:54 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 
+using namespace std;
 
+template<typename T>
+// const T / T const
+T min(T arr[], size_t size) {
+    T min = arr[0];
+    for (size_t i = 0; i < size; i++){
+        if (arr[i] < min) { min = arr[i]; }
+    }
+    return min;
+}
+
+template<typename T>
+// const T / T const
+T max(T arr[], size_t size) {
+    T max = arr[0];
+    for (size_t i = 0; i < size; i++){
+        if (arr[i] > max) { max = arr[i]; }
+    }
+    return max;
+}
 
 
 
 int main(void) {
+
+    long long arr[] = {-1, 1, 2, 5, 3, 4};
+
+    cout << "min: " << min(arr, 6) << endl;
+    cout << "max: " << max(arr, 6) << endl;
 
     return 0;
 }
